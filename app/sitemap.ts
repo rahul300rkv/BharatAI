f822db1563c3298d5cf8a609ced0defc624b7813
rahul300rkv <65898189+rahul300rkv@users.noreply.github.com>
2026-04-01 23:11:08 +0530

    Create sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://bharat-ai2.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://bharat-ai2.vercel.app/classroom',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+  ];
+}j
