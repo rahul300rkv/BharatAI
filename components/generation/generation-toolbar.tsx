e8b29aa1d5afa61367eed6c172b3692319677f71
rahul300rkv <65898189+rahul300rkv@users.noreply.github.com>
2026-06-21 00:49:28 +0530

    Update language display in generation toolbar

--- components/generation/generation-toolbar.tsx
+++ components/generation/generation-toolbar.tsx
@@ -365,7 +365,7 @@ export function GenerationToolbar({
             className={pillMuted}
           >
             <Globe className="size-3.5" />
-            <span>{language === 'hi-IN' ? '中文' : 'EN'}</span>
+            <span>{language === 'hi-IN' ? 'HI' : 'EN'}</span>
           </button>
         </TooltipTrigger>
         <TooltipContent>{t('toolbar.languageHint')}</TooltipContent>
